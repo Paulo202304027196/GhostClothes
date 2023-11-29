dfa7f43ce6d2a9e20a7da48c76a99b025d758a05
PauloSantos04 <131309122+Paulo202304027196@users.noreply.github.com>
2023-11-29 20:01:36 -0300

    Create submit.php

---
+++ docs/submit.php
@@ -0,0 +1,14 @@
+<?php
+    $dbhost = 'Localhost';
+    $dbusername = 'root';
+    $dbPassword = '';
+    $dbName = 'GhostClothes';
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+
+?>
